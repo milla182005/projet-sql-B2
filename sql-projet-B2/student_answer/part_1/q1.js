e3b90f42c41dd4ac42c4ed8f8a5ae9bfe7863152
lingrid <ingrid.lare@ynov.com>
2024-11-05 11:32:08 +0100

    new commit

--- sql-projet-B2/student_answer/part_1/q1.js
+++ sql-projet-B2/student_answer/part_1/q1.js
@@ -1,3 +1,8 @@
-const q1 = ``;
+const q1 = `SELECT pokedex_number AS  'N°' 
+                   name AS 'Pokemon'
+                   description AS 'Description'
+            FROM pokemon
+            WHERE LENGTH(description) < 75
+            ORDER BY pokedex_number ASC;`;
 
 module.exports = q1;
