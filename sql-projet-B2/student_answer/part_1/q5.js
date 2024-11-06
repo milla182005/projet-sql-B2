4562e923c5265af253d3d29cc21f8c3bc890efdd
lingrid <ingrid.lare@ynov.com>
2024-11-06 17:50:35 +0100

    new commit

--- sql-projet-B2/student_answer/part_1/q5.js
+++ sql-projet-B2/student_answer/part_1/q5.js
@@ -1,3 +1,15 @@
-const q5 = ``;
+const q5 = `SELECT 
+    pokemon.pokedex_number AS 'N°', 
+    pokemon.name AS 'Pokemon'
+FROM 
+    pokemon
+JOIN 
+    pokemon_ability ON pokemon.pokemon_id = pokemon_ability.pokemon_id  
+JOIN 
+    ability  ON pokemon_ability.ability_id = ability.ability_id  
+WHERE 
+    ability.name = 'Coloforce'  
+ORDER BY 
+    pokemon.pokedex_number ASC;  `;
 
 module.exports = q5;
