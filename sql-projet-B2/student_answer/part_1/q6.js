c4c8d1e725a65c16a0fbbf65b12c04658c162228
lingrid <ingrid.lare@ynov.com>
2024-11-07 11:46:02 +0100

    new commit

---
+++ sql-projet-B2/student_answer/part_1/q6.js
@@ -0,0 +1,15 @@
+const q6 = `SELECT 
+    pokemon.pokedex_number AS 'N°', 
+    pokemon.name AS 'Pokemon'
+FROM 
+    pokemon
+JOIN 
+    pokemon_ability ON pokemon.pokemon_id = pokemon_ability.pokemon_id  
+JOIN 
+    ability  ON pokemon_ability.ability_id = ability.ability_id  
+WHERE 
+    ability.name = 'Coloforce'  
+ORDER BY 
+    pokemon.pokedex_number ASC;  `;
+
+module.exports = q6;
