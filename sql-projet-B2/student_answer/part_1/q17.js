2d739219cef7cd6f68a3d615bee847428bcf1449
lingrid <ingrid.lare@ynov.com>
2024-11-07 20:47:56 +0100

    new commit

--- sql-projet-B2/student_answer/part_1/q17.js
+++ sql-projet-B2/student_answer/part_1/q17.js
@@ -1,3 +1,28 @@
-const q17 = ``;
+const q17 = `SELECT 
+    t.name AS 'Type', 
+    COUNT(m.move_id) AS 'Nombre capacités',
+
+    ROUND(SUM(CASE WHEN m.accuracy = 100 THEN 1 ELSE 0 END) * 100.0 / COUNT(m.move_id), 2) AS 'Capacité précision 100%',
+    
+    ROUND(SUM(CASE WHEN m.category = 'Physique' THEN 1 ELSE 0 END) * 100.0 / COUNT(m.move_id), 2) AS 'Pourcentage capacité physique',
+
+    ROUND(SUM(CASE WHEN m.category = 'Spéciale' THEN 1 ELSE 0 END) * 100.0 / COUNT(m.move_id), 2) AS 'Pourcentage capacité spéciale',
+
+    ROUND(SUM(CASE WHEN m.category = 'Statut' THEN 1 ELSE 0 END) * 100.0 / COUNT(m.move_id), 2) AS 'Pourcentage capacité statut',
+
+    ROUND(SUM(CASE WHEN m.power <= 40 AND m.power IS NOT NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(m.move_id), 2) AS 'Pourcentage capacité faible',
+
+    
+    ROUND(SUM(CASE WHEN m.power >= 100 AND m.power IS NOT NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(m.move_id), 2) AS 'Pourcentage capacité forte'
+
+FROM 
+    type t
+JOIN 
+    move m ON t.type_id = m.type_id
+GROUP BY 
+    t.name
+ORDER BY 
+    t.name;
+`;
 
 module.exports = q17;
---
+++ sql-projet-B2/student_answer/part_2/create.sql
@@ -0,0 +1,114 @@
+CREATE DATABASE IF NOT EXISTS movie_db;
+USE movie_db;
+
+CREATE TABLE genre (
+    genre_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE keyword (
+    keyword_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE language (
+    language_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    code VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE movie (
+    movie_id INT AUTO_INCREMENT PRIMARY KEY,
+    adult BOOLEAN,
+    budget INT,
+    original_language VARCHAR,
+    original_title VARCHAR,
+    overview VARCHAR,
+    popularity REAL,
+    status VARCHAR,
+    title VARCHAR(255) NOT NULL,
+    vote_average REAL,
+    vote_count INT
+    
+);
+
+CREATE TABLE departement (
+    departement_id INT AUTO_INCREMENT PRIMARY KEY,
+    departement_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE production_company (
+    company_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    origin_country VARCHAR(100)
+);
+
+CREATE TABLE gender (
+    gender_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE person (
+    person_id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR,
+    lastName VARCHAR,
+    birthDate VARCHAR,
+    popularity REAL
+    gender_id INT,
+    FOREIGN KEY (gender_id) REFERENCES gender(gender_id)
+);
+
+CREATE TABLE movie_genre (
+    movie_id INT NOT NULL,
+    genre_id INT NOT NULL,
+    PRIMARY KEY (movie_id, genre_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id) ON DELETE CASCADE
+);
+
+CREATE TABLE movie_keyword (
+    movie_id INT NOT NULL,
+    keyword_id INT NOT NULL,
+    PRIMARY KEY (movie_id, keyword_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    FOREIGN KEY (keyword_id) REFERENCES keyword(keyword_id) ON DELETE CASCADE
+);
+
+CREATE TABLE movie_language (
+    movie_id INT NOT NULL,
+    language_id INT NOT NULL,
+    PRIMARY KEY (movie_id, language_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    FOREIGN KEY (language_id) REFERENCES language(language_id) ON DELETE CASCADE
+);
+
+CREATE TABLE movie_company (
+    movie_id INT NOT NULL,
+    company_id INT NOT NULL,
+    PRIMARY KEY (movie_id, company_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    FOREIGN KEY (company_id) REFERENCES production_company(company_id) ON DELETE CASCADE
+);
+
+CREATE TABLE movie_cast (
+    movie_cast_id INT,
+    movie_id INT NOT NULL,
+    gender_id INT NOT NULL,
+    person_id INT NOT NULL,
+    character_name VARCHAR(255) NOT NULL,
+    cast_order INT,
+    PRIMARY KEY (movie_cast_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE
+);
+
+CREATE TABLE movie_crew (
+    movie_id INT NOT NULL,
+    person_id INT NOT NULL,
+    departement_id INT NOT NULL,
+    job VARCHAR(100) NOT NULL,
+    PRIMARY KEY (movie_id, person_id, departement_id),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE,
+    FOREIGN KEY (departement_id) REFERENCES departement(departement_id) ON DELETE CASCADE
+);
