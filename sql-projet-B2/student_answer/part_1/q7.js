4469bd2b5490b43bdb1d70363e19fbd5a458af6d
lingrid <ingrid.lare@ynov.com>
2024-11-07 12:33:54 +0100

    new commit

--- sql-projet-B2/student_answer/part_1/q7.js
+++ sql-projet-B2/student_answer/part_1/q7.js
@@ -1,3 +1,20 @@
-const q7 = ``;
+const q7 = `SELECT 
+    pokemon.pokedex_number AS 'N°',
+    pokemon.name AS 'Nom du pokemon',
+    t1.name AS 'Type 1',
+    COALESCE(t2.name, 'N/A') AS 'Type 2'
+FROM 
+    pokemon 
+JOIN 
+    pokemon_type pt1 ON pokemon.pokemon_id = pt1.pokemon_id AND pt1.slot = 1
+JOIN 
+    type t1 ON pt1.type_id = t1.type_id
+LEFT JOIN 
+    pokemon_type pt2 ON pokemon.pokemon_id = pt2.pokemon_id AND pt2.slot = 2
+LEFT JOIN 
+    type t2 ON pt2.type_id = t2.type_id
+ORDER BY 
+    pokemon.pokedex_number ASC;
+`;
 
 module.exports = q7;
---
+++ sql-projet-B2/student_answer/part_1/q8.js
@@ -0,0 +1,3 @@
+const q8 = ``;
+
+module.exports = q8;
